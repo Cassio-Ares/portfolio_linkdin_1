07c646dfe91dba823b269d94f8a47ce4e612768e
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-05-23 17:25:08 -0300

    Ajust text

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
   return (
     <Div>
       Olá! Sou Cássio, um entusiasta da programação realizando meu sonho de me tornar
-      um desenvolvedor Fullstack após concluir um curso excepcional na DNC (Digital Nomad Camp).
+      um desenvolvedor Fullstack após concluir um curso excepcional na DNC .
       Minha paixão pela programação é impulsionada pela curiosidade e pelo desejo de criar
       soluções inovadoras que melhorem a vida das pessoas. Busco constantemente aprimorar
       minhas habilidades técnicas e desenvolver uma mentalidade resiliente para enfrentar
